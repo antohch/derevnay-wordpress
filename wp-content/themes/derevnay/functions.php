88bea696ae42754c7220e8c5ca364023dea8f4a0
Anton Kadantsev <kadantsev.anton@yandex.ru>
2016-03-08 15:13:20 +0300

    начал работать над телевоном

--- wp-content/themes/derevnay/functions.php
+++ wp-content/themes/derevnay/functions.php
@@ -28,3 +28,37 @@ function kad_scripts_footer(){
 
 //меню
 register_nav_menu('menu', 'Верхнее меню');
+
+//новый пункт в админке, телефон
+/*add_action('admin', 'kad_add_phone');
+
+function kad_add_phone(){
+	add_settings_field(
+		'phone',
+		'Телефон',
+		'display_phone',
+		'general'
+	);
+	register_setting(
+		'general',
+		'my_phone'
+	);
+}
+function display_phone(){
+	echo "<input type='text' class='regular-text' name='my_phone[cyti1]' value='".esc_attr(get_option('my_phone[cyti1]'))."'>";
+	echo "<input type='text' class='regular-text' name='my_phone[phone1]' value='".esc_attr(get_option('my_phone[phonei1]'))."'>";
+}*/
+function my_more_options(){
+	add_settings_field(
+		'phone',//id
+		'Телефон',//название поле
+		'display_phone',//callback
+		'general');//'general'-где выводить. Cоздаем поле опции
+	register_setting(
+		'general',//название группу к какой будет принадлежать опция
+		'my_phone');//'my_phone' - название опции, которая будет сохраняться в БД. Регистрирует новую опцию и callback функции (функцию обратного вызова) для обработки значения опции при её сохранении в БД
+}
+add_action('admin_init', 'my_more_options');//привязать к хуку
+function display_phone(){
+	echo "<input type='text' class='regular-text' name='my_phone' value='".esc_attr(get_option('my_phone'))."'>";
+}
