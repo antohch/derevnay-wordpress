1d9a278fcbedb0d925e79cc73ac1a64fc19c20c5
Anton Kadantsev <kadantsev.anton@yandex.ru>
2016-03-10 12:41:17 +0300

    закончил посты на главной

--- wp-content/themes/derevnay/index.php
+++ wp-content/themes/derevnay/index.php
@@ -7,13 +7,8 @@
 		</div>
 	</div>
 	<div class="slider-shadow"></div>
-	<div class="info-all">
-		<div class="info">
-			<a href="#">
-			<img src="<?php bloginfo('template_url')?>/images/content1.jpg" title="" alt="" />
-			<span>Трансляции футбольных матчей</span>
-			</a>
-		</div>
+	
+		<!--<
 		<div class="info">
 			<a href="#">
 			<img src="<?php bloginfo('template_url')?>/images/content2.jpg" title="" alt="" />
@@ -25,8 +20,29 @@
 			<img src="<?php bloginfo('template_url')?>/images/content3.jpg" title="" alt="" />
 			<span><br />События</span>
 			</a>
-		</div>
-	</div>
+		-->
+		<?php 
+		$post_home = new WP_Query(array(
+			'category_name' => 'for_home',
+			'post_status' => 'publish',
+			'posts_per_page' => 3,
+			'order' => 'ASC'
+		));
+		?>
+		<?php if($post_home->have_posts()): ?>
+			<div class="info-all">
+			<?php while($post_home->have_posts()): ?>
+				<?php $post_home->the_post(); ?>
+				<div class="info">
+					<a href="<?php the_permalink(); ?>">
+					<?php echo get_the_post_thumbnail(); ?>
+					<span><?php the_title(); ?></span>
+					</a>
+				</div>
+			<?php endwhile; ?>
+			</div>
+		<?php endif; wp_reset_postdata();?>
+	
 	<div class="about">
 		<h2>О кафе</h2>
 		<p>Text text text text text text text text text text text text text text </p>
